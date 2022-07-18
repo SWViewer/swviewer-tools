d33bc71b3060b0eb331fa7d157c6ffec6141576a
Siarhei <iluvatar@toolforge,org>
2022-07-18 16:52:37 +0300

    small fix

--- UpdaterNameList.py
+++ UpdaterNameList.py
@@ -5,10 +5,15 @@ import json
 import time
 from urllib.request import urlopen
 import toolforge
+smallffff
+
+sql = """SELECT site_global_key AS wiki, TRIM(trailing '.org' 
+FROM trim(leading '.' FROM REVERSE(site_domain))) AS domain FROM sites"
+"""
 
 conn = toolforge.connect('metawiki_p')
 with conn.cursor() as cur:
-    cur.execute("SELECT site_global_key AS wiki, trim(trailing '.org' from trim(leading '.' from reverse(site_domain))) AS domain FROM sites")
+    cur.execute(sql)
     res = cur.fetchall()
     cur.close()
 
@@ -42,8 +47,11 @@ if len(all_wikis) >= 500:
 
     ex = []
     for w in all_wikis:
-        url = "https://" + w[1] + ".org/w/api.php?action=query&uselang=en&meta=siteinfo&format=json&siprop=general&utf8=1"
-        a = w[0].replace("wikimedia", "").replace("wikiquote", "").replace("wiktionary", "").replace("wikivoyage", "").replace("wikisource", "").replace("wikiversity", "").replace("wikibooks", "").replace("wikinews", "").replace("wiki", "")
+        url = "https://{0}.org/w/api.php?action=query&uselang=en&meta=siteinfo&format=json&siprop=general&utf8=1"\
+            .format(w[1])
+        a = w[0].replace("wikimedia", "").replace("wikiquote", "").replace("wiktionary", "").replace("wikivoyage", "")\
+            .replace("wikisource", "").replace("wikiversity", "").replace("wikibooks", "").replace("wikinews", "")\
+            .replace("wiki", "")
         res = urlopen(url).read()
         res = json.loads(res.decode("utf-8"))
         if a.replace("_", "-") != res["query"]["general"]["lang"]:
@@ -51,37 +59,41 @@ if len(all_wikis) >= 500:
             ex.append([res["query"]["general"]["lang"], w[0]])
         time.sleep(1)
 
-    
     if len(ex) > 5:
         with open('public_html/lists/exLangs.txt', 'w') as exl:
             for e in ex:
-                exl.write(e[0] + "," + e[1] + '\n')
+                exl.write("{0},{1}\n".format(e[0], e[1]))
             exl.close()
 
 langs = []
 for a in all_wikis:
-        a = a[0].replace("wikimedia", "").replace("wikiquote", "").replace("wiktionary", "").replace("wikivoyage", "").replace("wikisource", "").replace("wikiversity", "").replace("wikibooks", "").replace("wikinews", "").replace("wiki", "")
-        if a not in langs:
-            langs.append(a)
+    a = a[0].replace("wikimedia", "").replace("wikiquote", "").replace("wiktionary", "").replace("wikivoyage", "")\
+        .replace("wikisource", "").replace("wikiversity", "").replace("wikibooks", "").replace("wikinews", "")\
+        .replace("wiki", "")
+    if a not in langs:
+        langs.append(a)
 
 all_langs = []
 if len(langs) > 10:
-    X = [langs[i:i+49] for i in range(0, len(langs), 49)]
+    X = [langs[i:i + 49] for i in range(0, len(langs), 49)]
     for m in X:
         raw = ""
         for z in m:
             raw += z.replace("_", "-") + "|"
-        req = urlopen("https://www.mediawiki.org/w/api.php?action=query&licode=" + raw + "&format=json&utf8=1&meta=languageinfo&liprop=bcp47%7Cautonym%7Cname%7Ccode%7Cname&uselang=en").read()
+        req = urlopen(
+            "https://www.mediawiki.org/w/api.php?action=query&licode={0}&format=json&utf8=1&meta=languageinfo"
+            "&liprop=bcp47%7Cautonym%7Cname%7Ccode%7Cname&uselang=en".format(raw)).read()
         req = json.loads(req.decode("utf-8"))
         for z in m:
             try:
-                all_langs.append([z, z + " - " + req["query"]["languageinfo"][z.replace("_", "-")]["name"]])
-            except:
+                all_langs.append([z, "{0} - {1}".format(z, req["query"]["languageinfo"][z.replace("_", "-")]["name"])])
+            except Exception as e:
+                print("Languageinfo error: {0}".format(e))
                 pass
         time.sleep(5)
 
 if len(all_langs) > 50:
     with open('public_html/lists/namesLangs.txt', 'w') as fl:
-        for l in all_langs:
-            fl.write(l[0] + "," + l[1] + '\n')
+        for lang in all_langs:
+            fl.write("{0},{1}\n".format(lang[0], lang[1]))
         fl.close()
